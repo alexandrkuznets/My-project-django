578ac5620a9b6bdfa063990fdefd693cb54bbf8b
Aleksander Kuznetsov <kosogor2709@mail.ru>
2025-11-06 17:32:48 +0300

    first

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,29 +9,34 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import logging.config
+from os import getenv
 from pathlib import Path
-
+from logging import config
 from django.conf.global_settings import LOGIN_REDIRECT_URL, MEDIA_URL, LOGGING
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DATABASE_DIR = BASE_DIR / "database"
+DATABASE_DIR.mkdir(exist_ok=True)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i-hevq4^ufc+v2zh5i)y9ur*o*xv0265ixiez#d12s9d=w1%$('
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY",
+    'django-insecure-i-hevq4^ufc+v2zh5i)y9ur*o*xv0265ixiez#d12s9d=w1%$(',
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
-]
+] + getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
@@ -110,7 +115,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -194,33 +199,58 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
 }
 
-LOGFILE_NAME = BASE_DIR / "log.txt"
-LOGFILE_SIZE = 1 * 1024 * 1024
-LOGFILE_COUNT = 3
+# LOGFILE_NAME = BASE_DIR / "log.txt"
+# LOGFILE_SIZE = 1 * 1024 * 1024
+# LOGFILE_COUNT = 3
 
-LOGGING = {
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "formatters": {
+#         "verbose": {
+#             "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+#         },
+#     },
+#     "handlers": {
+#         "console": {
+#             "class": "logging.StreamHandler",
+#             "formatter": "verbose",
+#         },
+#         "logfile": {
+#             "class": "logging.handlers.RotatingFileHandler",
+#             "filename": LOGFILE_NAME,
+#             "maxBytes": LOGFILE_SIZE,
+#             "backupCount": LOGFILE_COUNT,
+#             "formatter": "verbose",
+#         },
+#     },
+#     "root": {
+#         "handlers": ["console", "logfile", ],
+#         "level": "INFO",
+#     },
+# }
+
+LOGLEVEL = getenv("DJANGO_LOGLEVEL", "info").upper()
+
+logging.config.dictConfig({
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "verbose": {
-            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        "console": {
+            "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s",
         },
     },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-        "logfile": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": LOGFILE_NAME,
-            "maxBytes": LOGFILE_SIZE,
-            "backupCount": LOGFILE_COUNT,
-            "formatter": "verbose",
+            "formatter": "console",
         },
     },
-    "root": {
-        "handlers": ["console", "logfile", ],
-        "level": "INFO",
+    "loggers": {
+        "": {
+            "level": LOGLEVEL,
+            "handlers": ["console",],
+        },
     },
-}
+
+})
